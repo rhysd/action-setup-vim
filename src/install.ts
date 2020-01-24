add932ac0e50d9ea400dc4d20bc9168f9ff72f05
rhysd <lin90162@yahoo.co.jp>
2020-01-24 12:58:51 +0900

    add first skeleton for each platform

--- src/install.ts
+++ src/install.ts
@@ -1,10 +1,21 @@
-// import * as core from '@actions/core';
+import * as core from '@actions/core';
 import { Config } from './config';
+import { install as installOnLinux } from './install_linux';
+import { install as installOnMacOs } from './install_macos';
+import { install as installOnWindows } from './install_windows';
 
 export interface Installed {
     executable: string;
 }
 
-export async function install(config: Config): Promise<Installed> {
-    throw new Error(`Not implemented: ${JSON.stringify(config)}`);
+export function install(config: Config): Promise<Installed> {
+    core.debug(`Detected operating system: ${config.os}`);
+    switch (config.os) {
+        case 'linux':
+            return installOnLinux(config);
+        case 'macos':
+            return installOnMacOs(config);
+        case 'windows':
+            return installOnWindows(config);
+    }
 }
---
+++ src/install_windows.ts
@@ -0,0 +1,55 @@
+import * as core from '@actions/core';
+import { Installed } from './install';
+import { Config } from './config';
+
+async function installVimStable(): Promise<Installed> {
+    core.debug('Installing stable Vim on Windows');
+    throw new Error('Not implemented');
+}
+
+async function installVimNightly(): Promise<Installed> {
+    core.debug('Installing nightly Vim on Windows');
+    throw new Error('Not implemented');
+}
+
+async function installVim(ver: string): Promise<Installed> {
+    core.debug(`Installing Vim version '${ver}' on Windows`);
+    throw new Error(`Installing Vim of specific version '${ver}' is not supported yet`);
+}
+
+async function installNeovimStable(): Promise<Installed> {
+    core.debug('Installing stable Neovim on Windows');
+    throw new Error('Not implemented');
+}
+
+async function installNeovimNightly(): Promise<Installed> {
+    core.debug('Installing nightly Neovim on Windows');
+    throw new Error('Not implemented');
+}
+
+async function installNeovim(ver: string): Promise<Installed> {
+    core.debug(`Installing Neovim version '${ver}' on Windows`);
+    throw new Error(`Installing NeoVim of specific version '${ver}' is not supported yet`);
+}
+
+export function install(config: Config): Promise<Installed> {
+    if (config.neovim) {
+        switch (config.version) {
+            case 'stable':
+                return installVimStable();
+            case 'nightly':
+                return installVimNightly();
+            default:
+                return installVim(config.version);
+        }
+    } else {
+        switch (config.version) {
+            case 'stable':
+                return installNeovimStable();
+            case 'nightly':
+                return installNeovimNightly();
+            default:
+                return installNeovim(config.version);
+        }
+    }
+}
