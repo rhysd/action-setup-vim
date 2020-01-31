18caef22cfc3ec6678ab6adec982c9f8aa9ce2df
rhysd <lin90162@yahoo.co.jp>
2020-01-31 15:27:45 +0900

    fix $PATH separator is wrong on Windows

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,9 @@ async function main() {
     const config = loadConfigFromInputs();
     console.log('Extracted configuration:', config);
 
+    const pathSep = process.platform === 'win32' ? ';' : ':';
     const installed = await install(config);
-    console.log(`::set-env name=PATH::${installed.bin}:${process.env.PATH}`);
+    console.log(`::set-env name=PATH::${installed.bin}${pathSep}${process.env.PATH}`);
     core.debug(`'${installed.bin}' was set to $PATH`);
 
     await validateInstallation(installed);
