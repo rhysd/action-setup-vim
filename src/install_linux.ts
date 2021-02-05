dfe72b0dfa9096a9b45fa06762a74e9b711ffd99
rhysd <lin90162@yahoo.co.jp>
2021-02-05 21:49:21 +0900

    use vim-gtk3 package on Ubuntu 19.04 or later for stable Vim

--- src/install_linux.ts
+++ src/install_linux.ts
@@ -1,14 +1,36 @@
 import * as core from '@actions/core';
+import { getUbuntuVersion } from 'ubuntu-version';
 import type { Installed } from './install';
 import type { Config } from './config';
 import { exec } from './shell';
 import { buildVim } from './vim';
 import { downloadNeovim, downloadStableNeovim } from './neovim';
 
+async function isUbuntu18OrEarlier(): Promise<boolean> {
+    const ver = await getUbuntuVersion();
+    if (ver === null) {
+        core.error('Trying to install apt package but current OS is not Ubuntu');
+        return false; // Should be unreachable
+    }
+
+    core.debug(`Ubuntu system information ${JSON.stringify(ver)}`);
+
+    const m = ver.release.match(/^(\d+)\./);
+    if (m === null) {
+        core.error(`Unexpected 'Release' value of OS info: ${ver.release}`);
+        return false;
+    }
+
+    const majorVer = parseInt(m[1], 10);
+    core.debug(`Ubuntu major version: ${majorVer}`);
+    return majorVer <= 18;
+}
+
 async function installVimStable(): Promise<Installed> {
     core.debug('Installing stable Vim on Linux using apt');
+    const pkg = (await isUbuntu18OrEarlier()) ? 'vim-gnome' : 'vim-gtk3';
     await exec('sudo', ['apt', 'update', '-y']);
-    await exec('sudo', ['apt', 'install', '-y', 'vim-gtk3']);
+    await exec('sudo', ['apt', 'install', '-y', pkg]);
     return {
         executable: 'vim',
         binDir: '/usr/bin',
