24837ab7c76beb158399fa4c79e7ce6974797302
linrongbin16 <linrongbin16@outlook.com>
2024-03-29 13:10:38 +0800

    fix test

--- test/neovim.ts
+++ test/neovim.ts
@@ -138,7 +138,16 @@ describe('Neovim installation', function () {
             A.equal(assetDirName('v0.7.1', 'macos'), 'nvim-macos');
             A.equal(assetDirName('v0.10.0', 'macos'), 'nvim-macos');
             A.equal(assetDirName('v1.0.0', 'macos'), 'nvim-macos');
-            A.equal(assetDirName('nightly', 'macos'), 'nvim-macos');
+            switch (process.arch) {
+                case 'arm64':
+                    A.equal(assetDirName('nightly', 'macos'), 'nvim-macos-arm64');
+                    break;
+                case 'x64':
+                    A.equal(assetDirName('nightly', 'macos'), 'nvim-macos-x86_64');
+                    break;
+                default:
+                    A.equal(assetDirName('nightly', 'macos'), 'nvim-macos');
+            }
             A.equal(assetDirName('stable', 'macos'), 'nvim-macos');
         });
     });
