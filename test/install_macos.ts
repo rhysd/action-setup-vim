c437279a4dd235b1ffdf92f2e6cd33dbdc4a8e2d
rhysd <lin90162@yahoo.co.jp>
2025-11-02 17:25:12 +0900

    test the case where the conflict workaround on x86_64 doesn't happen

--- test/install_macos.ts
+++ test/install_macos.ts
@@ -6,25 +6,23 @@ import { ExecStub } from './helper.js';
 describe('Installation on macOS', function () {
     const stub = new ExecStub();
     let installMocked: typeof install;
+    let dummyRealPath: string;
 
     before(async function () {
-        const fsMock = {
-            stat: (): Promise<unknown> => Promise.resolve({ isDirectory: () => true }),
-            lstat: (): Promise<unknown> => Promise.resolve({ isSymbolicLink: () => true }),
-        };
         const { install } = await stub.importWithMock('../src/install_macos.js', {
-            'fs/promises': fsMock,
+            'fs/promises': {
+                realpath: (): Promise<string> => Promise.resolve(dummyRealPath),
+            },
             '@actions/io': {
-                rmRF(): Promise<void> {
-                    return Promise.resolve();
-                },
+                rmRF: (): Promise<void> => Promise.resolve(),
             },
         });
         installMocked = install;
     });
 
-    afterEach(function () {
+    beforeEach(function () {
         stub.reset();
+        dummyRealPath = '/Library/Frameworks/Python.framework/Versions/dummy';
     });
 
     it('installs stable Neovim from Homebrew', async function () {
@@ -78,10 +76,30 @@ describe('Installation on macOS', function () {
         A.equal(installed.executable, 'vim');
         A.equal(installed.binDir, '/usr/local/bin');
 
-        A.deepEqual(stub.called[0], ['brew', ['unlink', 'python@3.13', '--quiet']]);
-        A.deepEqual(stub.called[1], ['brew', ['link', 'python@3.13', '--quiet', '--overwrite']]);
+        A.deepEqual(stub.called[0], ['brew', ['unlink', 'python@3', '--quiet']]);
+        A.deepEqual(stub.called[1], ['brew', ['link', 'python@3', '--quiet', '--overwrite']]);
         A.deepEqual(stub.called[2], ['brew', ['update', '--quiet']]);
         A.deepEqual(stub.called[3], ['brew', ['install', 'macvim', '--quiet']]);
         A.equal(stub.called.length, 4);
+    });
+
+    it('does not do package conflict workaround when executables are not linked to Python.Framework', async function () {
+        dummyRealPath = '/usr/local/opt/python@3/bin/dummy';
+        const config: Config = {
+            version: 'stable',
+            neovim: false,
+            os: 'macos',
+            arch: 'x86_64',
+            configureArgs: null,
+            token: null,
+        };
+
+        const installed = await installMocked(config);
+        A.equal(installed.executable, 'vim');
+        A.equal(installed.binDir, '/usr/local/bin');
+
+        A.deepEqual(stub.called[0], ['brew', ['update', '--quiet']]);
+        A.deepEqual(stub.called[1], ['brew', ['install', 'macvim', '--quiet']]);
+        A.equal(stub.called.length, 2);
     });
 });
