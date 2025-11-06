939e8e9b8debd1f90466aefd510b3dd0c51265b7
rhysd <lin90162@yahoo.co.jp>
2025-11-06 13:19:06 +0900

    validate `vim-dir` output in post-action check script

--- scripts/post_action_check.ts
+++ scripts/post_action_check.ts
@@ -2,31 +2,29 @@ import { homedir } from 'node:os';
 import * as path from 'node:path';
 import { strict as assert } from 'node:assert';
 import { spawnSync } from 'node:child_process';
-import { existsSync, readdirSync } from 'node:fs';
+import { existsSync, readdirSync, readFileSync, realpathSync } from 'node:fs';
 import process from 'node:process';
 
 function log(...args: unknown[]): void {
     console.log('[post_action_check]:', ...args);
 }
 
-function ok(x: unknown, m?: string): asserts x {
-    assert.ok(x, m);
-}
-
 interface Args {
     neovim: boolean;
     version: string;
-    output: string;
+    executable: string;
+    vimdir: string;
 }
 
 function parseArgs(args: string[]): Args {
     if (args.length !== 5) {
-        throw new Error('3 arguments must be set: `node ./scripts/post_action_check.js {neovim?} {version} {output}`');
+        throw new Error('3 arguments must be set: `node ./scripts/post_action_check.js {neovim?} {version} {outputs}`');
     }
-
     const neovim = args[2].toLowerCase() === 'true';
-
-    return { neovim, version: args[3], output: args[4] };
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const outputs = JSON.parse(args[4]);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-member-access
+    return { neovim, version: args[3], executable: outputs.executable, vimdir: outputs['vim-dir'] };
 }
 
 // e.g. ~/vim-stable/vim91
@@ -97,6 +95,18 @@ function expectedExecutable(neovim: boolean, ver: string): string {
     throw new Error(`Unexpected platform '${process.platform}'`);
 }
 
+function getVimVariable(variable: string, exe: string, neovim: boolean): string {
+    const args = ['-c', 'redir! > tmp_command_output.txt', '-c', `echon ${variable}`, '-c', 'redir END', '-c', 'qa!'];
+    if (neovim) {
+        args.unshift('--headless', '--clean');
+    } else {
+        args.unshift('--not-a-term', '-u', 'NONE', '-i', 'NONE', '-N', '-n', '-e', '-s', '--noplugin');
+    }
+    const proc = spawnSync(exe, args, { timeout: 5000 });
+    assert.equal(proc.error, undefined, `stderr=${proc.stderr.toString()}`);
+    return readFileSync('tmp_command_output.txt', 'utf-8').trim();
+}
+
 function main(): void {
     log('Running with argv:', process.argv);
 
@@ -104,16 +114,16 @@ function main(): void {
     log('Command line arguments:', args);
 
     const exe = expectedExecutable(args.neovim, args.version);
-    log('Validating output. Expected executable:', exe);
-    assert.equal(exe, args.output);
+    log('Validating executable path. Expected executable:', exe);
+    assert.equal(exe, args.executable);
     assert.ok(existsSync(exe));
 
     const bin = path.dirname(exe);
     log(`Validating '${bin}' is in $PATH`);
-    ok(process.env['PATH']);
+    assert.ok(process.env['PATH']);
     const pathSep = process.platform === 'win32' ? ';' : ':';
     const paths = process.env['PATH'].split(pathSep);
-    ok(paths.includes(bin), `'${bin}' is not included in '${process.env['PATH']}'`);
+    assert.ok(paths.includes(bin), `'${bin}' is not included in '${process.env['PATH']}'`);
 
     log('Validating executable');
     const proc = spawnSync(exe, ['-N', '-c', 'quit'], { timeout: 5000 });
@@ -129,29 +139,43 @@ function main(): void {
     assert.equal(ver.status, 0, `stderr: ${stderr}`);
     assert.equal(ver.signal, null, `stderr: ${stderr}`);
     const stdout = ver.stdout.toString();
-
     if (args.version !== 'stable' && args.version !== 'nightly') {
         if (args.neovim) {
             const l = `NVIM ${args.version}`;
-            ok(stdout.includes(l), `First line '${l}' should be included in stdout: ${stdout}`);
+            assert.ok(stdout.includes(l), `First line '${l}' should be included in stdout: ${stdout}`);
         } else {
             const m = args.version.match(/^v(\d+\.\d+)\.(\d+)$/);
-            ok(m);
+            assert.ok(m);
             const major = m[1];
             const patch = parseInt(m[2], 10);
 
             const l = `VIM - Vi IMproved ${major}`;
-            ok(stdout.includes(l), `First line '${l}' should be included in stdout: ${stdout}`);
+            assert.ok(stdout.includes(l), `First line '${l}' should be included in stdout: ${stdout}`);
 
             // assert.match is not available since it is experimental
-            ok(
+            assert.ok(
                 stdout.includes(`Included patches: 1-${patch}`),
                 `Patch 1-${patch} should be included in stdout: ${stdout}`,
             );
         }
     } else {
         const editorName = args.neovim ? 'NVIM' : 'VIM - Vi IMproved';
-        ok(stdout.includes(editorName), `Editor name '${editorName}' should be included in stdout: ${stdout}`);
+        assert.ok(stdout.includes(editorName), `Editor name '${editorName}' should be included in stdout: ${stdout}`);
+    }
+
+    // TODO: Remove this `if` condition after the next release.
+    if (process.env['GITHUB_WORKFLOW'] !== 'Post-release check') {
+        log('Validating $VIM directory', args.vimdir);
+        const vimdir = realpathSync(args.vimdir);
+        if (vimdir !== args.vimdir) {
+            log('vim-dir path is resolved to', vimdir);
+        }
+        let expected = getVimVariable('$VIM', exe, args.neovim);
+        if (process.platform === 'win32') {
+            // Neovim mixes '\' and '/' in $VIM value (\path\to\nvim-nightly\share/nvim)
+            expected = path.win32.normalize(expected);
+        }
+        assert.equal(expected, vimdir);
     }
 
     log('OK');
